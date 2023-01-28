76101692022bc168c871820c42c482bb80276d8e
Chris Walker <Chris.r.walker271@outlook.com>
2023-01-27 19:40:39 -0500

    Refactored database selection and creation variables. Created a global database variable, seperated database and table creation function to allow better flexibility by users.
    TODO:
    Finalize database selection functionality
    Implement question category functionality
    Figure out how to prompt new entry inside main window.
    Create user input validation on new entry / deletion
    Generate 10 question quiz
    Track and display user progress.

--- DatabaseFunctions.py
+++ DatabaseFunctions.py
@@ -1,21 +1,44 @@
 #Functions involved with database manipulation. This program utilizes SQLite3 for database management.
 
 import sqlite3
-import tkinter
+from sqlite3 import Error
+from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import simpledialog
 import random
 
-# Create database if one is not available, then populate Question Bank table.
+global current_db
+
+# Method for selecting current database
+def select_current_database():
+    global current_db
+    current_db = filedialog.askopenfilename(title="Select a Question Bank", filetypes=(".db"))
+    print(current_db)
+
+# Create database if one is not available.
+def create_database():
+    try:
+        conn = None
+        user_bank_name = simpledialog.askstring(title="Name Question Bank", prompt="Please enter a topic for your new question bank.")
+        conn = sqlite3.connect(user_bank_name.replace(" ", "")+'.db')
+    except Error as e:
+        messagebox.showerror(title="An Error Occurred", message=str(e))
+    finally:
+        if conn:
+            conn.close
+
+# Tables will function as "topics", or "question categories" for the user. 
 def create_table():
     try:
-        conn = sqlite3.connect('QuestionBank.db')
+        select_current_database()
+        conn = sqlite3.connect(current_db)
         cursor = conn.cursor()
-        cursor.execute('CREATE TABLE QUESTION_BANK (''EntryID INTEGER PRIMARY KEY,''Question varchar(255),''Answer varchar(255)'');')
+        user_table_name = simpledialog.askstring(title="Create Question Category", prompt="Please enter a category name")
+        cursor.execute('CREATE TABLE (?) (''EntryID INTEGER PRIMARY KEY,''Question varchar(255),''Answer varchar(255)'');', user_table_name)
+    except Error as e:
+        messagebox.showerror(title="An Error Occured", message=str(e))
+    finally:
         conn.close()
-
-    except Exception as e:
-        messagebox.showerror(title="An Error Occurred", message=str(e))
 
 # Delete current Question Bank table
 def delete_table():  
